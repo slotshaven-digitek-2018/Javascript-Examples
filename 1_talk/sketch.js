92ab28fc256da1a4ac13dc8b187f17a51d8a44ca
artix2990 <31307559+artix2990@users.noreply.github.com>
2018-12-06 11:20:59 +0100

    Jeg har tilføjet billeder til når man siger forskellige ting

--- 1_talk/sketch.js
+++ 1_talk/sketch.js
@@ -11,17 +11,31 @@ Reference: http://ability.nyu.edu/p5.js-speech/
 ...*/
 
 
-	var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+	var myRec = new p5.SpeechRec("da"); // new P5.SpeechRec object
     var words;
     var igenKnap;
     var sentence = "";
     var leftDiv;
     var counter;
+    var img;
+    var dickbutt;
+    var david;
+    var emil;
+    var sid;
+    var idfk;
+    var skat;
 
 
 	function setup()
     {
-		createCanvas(400, 300);
+		createCanvas(500, 500);
+        dickbutt = loadImage("pictures/dickbutt.jpg");
+        david = loadImage("pictures/david.png");
+        emil = loadImage("pictures/emil.jpg");
+        idfk = loadImage("pictures/idfk.png");
+        sid = loadImage("pictures/sid.png");
+        skat = loadImage("pictures/skat.jpg");
+        
 
         igenKnap = createElement('button', 'Prøv igen');
         igenKnap.addClass("hidden");
@@ -37,7 +51,11 @@ Reference: http://ability.nyu.edu/p5.js-speech/
 	}  
 
 	function draw()
-	{
+	{ 
+        
+        
+      
+        
 		// why draw when you can talk || why talk when you can draw??
         if(sentence.includes("cirkel")||sentence.includes("cirkler"))
            {
@@ -45,6 +63,30 @@ Reference: http://ability.nyu.edu/p5.js-speech/
                ellipse(width/2,height/2,200,200);
            
            }
+       if(sentence.includes("hey")||sentence.includes("hej")||sentence.includes("hi"))
+            {
+          image(dickbutt, 0, height/10, dickbutt.width/10, dickbutt.height/10);
+            }
+        if(sentence.includes("og")||sentence.includes("han")||sentence.includes("at"))
+            {
+          image(sid, 250, height/3, sid.width/3, sid.height/3);
+            }
+        if(sentence.includes("det")||sentence.includes("er")||sentence.includes("en"))
+            {
+          image(idfk, 0, 200, idfk.width/4, idfk.height/4);
+            }
+        if(sentence.includes("på")||sentence.includes("til")||sentence.includes("med"))
+            {
+            image(emil, 250, 0, emil.width/7, emil.height/7);
+            }
+        if(sentence.includes("af")||sentence.includes("ikke")||sentence.includes("mig"))
+            {
+            image(david, 420, height/10, david.width/20, david.height/20);
+            }
+        if(sentence.includes("hemmeligheder"))
+            {
+            image(skat, 0, 0, skat.width/1, skat.height/1);
+            }
             if(sentence.includes("gul"))
                 document.body.style.backgroundColor = "yellow";
             if(sentence.includes("rød"))
