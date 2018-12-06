6084b632c46189662c92dc716eb581b7413f61a5
bjarkec <42930338+bjarkec@users.noreply.github.com>
2018-12-06 10:19:51 +0100

    Formatting and changed title

--- 1_talk/sketch.js
+++ 1_talk/sketch.js
@@ -11,65 +11,65 @@ Reference: http://ability.nyu.edu/p5.js-speech/
 ...*/
 
 
-	var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
-    var words;
-    var igenKnap;
-    var sentence = "";
-    var leftDiv;
-    var counter;
+var myRec = new p5.SpeechRec(); // new P5.SpeechRec object
+var words;
+var igenKnap;
+var sentence = "";
+var leftDiv;
+var counter;
 
 
-	function setup()
+function setup()
+{
+    createCanvas(400, 300);
+
+    igenKnap = createElement('button', 'Prøv igen');
+    igenKnap.addClass("hidden");
+    igenKnap.mousePressed(doItAgain);
+
+
+    words = createElement('div', "<h2>Alexa 2.0</h2>");
+    words.addClass("words");
+    words.attribute('id', 'words');
+
+    myRec.onResult = showResult;
+    myRec.start();
+}  
+
+function draw()
+{
+    // why draw when you can talk || why talk when you can draw??
+    if(sentence.includes("cirkel")||sentence.includes("cirkler"))
     {
-		createCanvas(400, 300);
+        stroke(173,216,230);
+        ellipse(width/2,height/2,200,200);
 
-        igenKnap = createElement('button', 'Prøv igen');
-        igenKnap.addClass("hidden");
-        igenKnap.mousePressed(doItAgain);
-
-        
-        words = createElement('div', "<h2>Sig noget</h2>");
-        words.addClass("words");
-        words.attribute('id', 'words');
-
-        myRec.onResult = showResult;
-		myRec.start();
-	}  
-
-	function draw()
-	{
-		// why draw when you can talk || why talk when you can draw??
-        if(sentence.includes("cirkel")||sentence.includes("cirkler"))
-           {
-               stroke(173,216,230);
-               ellipse(width/2,height/2,200,200);
-           
-           }
-            if(sentence.includes("gul"))
-                document.body.style.backgroundColor = "yellow";
-            if(sentence.includes("rød"))
-                document.body.style.backgroundColor = "red";
-            if(sentence.includes("orange"))
-               document.body.style.backgroundColor = "orange";
-            if(sentence.includes("grøn"))
-               document.body.style.backgroundColor = "green";  
-            //det sortner for mine øjne
-            if(sentence.includes("sort"))
-               document.body.style.backgroundColor = "black";  
-	}
-
-	function showResult()
-	{
-		if(myRec.resultValue==true) {
-            $("#words").fadeOut(10000);
-            sentence = myRec.resultString;
-			words.html("<p>" + sentence + "</p>", true);
-            igenKnap.addClass("shown");
-            setTimeout(function(){ location.reload(); }, 3000);
-		}
-	}
-
-    function doItAgain(){
-        location.reload();
     }
+    if(sentence.includes("gul"))
+        document.body.style.backgroundColor = "yellow";
+    if(sentence.includes("rød"))
+        document.body.style.backgroundColor = "red";
+    if(sentence.includes("orange"))
+        document.body.style.backgroundColor = "orange";
+    if(sentence.includes("grøn"))
+        document.body.style.backgroundColor = "green";  
+    //det sortner for mine øjne
+    if(sentence.includes("sort"))
+        document.body.style.backgroundColor = "black";  
+}
+
+function showResult()
+{
+    if(myRec.resultValue==true) {
+        $("#words").fadeOut(10000);
+        sentence = myRec.resultString;
+        words.html("<p>" + sentence + "</p>", true);
+        igenKnap.addClass("shown");
+        setTimeout(function(){ location.reload(); }, 3000);
+    }
+}
+
+function doItAgain(){
+    location.reload();
+}
 
